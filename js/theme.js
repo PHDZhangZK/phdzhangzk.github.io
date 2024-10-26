d19b7a48b7245136316be2b145aaa69e84f09c9f
Zhankun Zhang <zzkmails@163.com>
2024-10-26 16:30:51 +0800

    Update theme.js

--- js/theme.js
+++ js/theme.js
@@ -1,12 +1,21 @@
 const themeToggle = document.getElementById('theme-toggle');
-let theme = 'system'; // 默认跟随系统主题
+const themeIcon = document.getElementById('theme-icon');
+let theme = 'system';
 
-// 检查 localStorage 中是否存有用户选择的主题
+function updateIcon() {
+    if (theme === 'dark') {
+        themeIcon.className = 'fas fa-moon'; // 月亮图标
+    } else if (theme === 'light') {
+        themeIcon.className = 'fas fa-sun'; // 太阳图标
+    } else {
+        themeIcon.className = 'fas fa-adjust'; // 系统跟随图标
+    }
+}
+
 if (localStorage.getItem('theme')) {
     theme = localStorage.getItem('theme');
     setTheme(theme);
 } else {
-    // 如果没有，则设置为跟随系统
     setTheme('system');
 }
 
@@ -18,18 +27,16 @@ themeToggle.addEventListener('click', () => {
     } else {
         theme = 'dark';
     }
-
-    // 保存到 localStorage 中
     localStorage.setItem('theme', theme);
     setTheme(theme);
 });
 
 function setTheme(mode) {
     document.body.classList.remove('dark-mode', 'system-mode');
-
     if (mode === 'dark') {
         document.body.classList.add('dark-mode');
     } else if (mode === 'system') {
         document.body.classList.add('system-mode');
     }
+    updateIcon();
 }
